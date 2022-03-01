2edc025c81595753b85ad26a8aa7d19e05538e64
Pedro Donoso <pedro.donoso.r@usach.cl>
2022-03-01 00:02:28 -0300

    react

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ app.put("/todos/:id", async (req, res) => {
     try {
         const { id } = req.params;
         const { description } = req.body;
-        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description = $1 WHERE todo_id = $2",
             [description, id]
         );
         res.json("Todo was updated!");
