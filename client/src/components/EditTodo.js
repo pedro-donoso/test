a3bd9148f33627ec74d367215520f9c1576cb93b
Pedro Donoso <pedro.donoso.r@usach.cl>
2022-03-02 10:48:14 -0300

    edicion

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const EditTodo = () => {
-    return <h6>Editar Tareas</h6>;
+    return <Fragment>
+
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal"> 
+            Editar
+        </button>
+
+        <div class="modal" id="myModal">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Editar tarea</h4>
+                        <button type="button" class="close" data-dismiss="modal">
+                        &times;
+                        </button>
+                    </div>
+
+                    <div class="modal-body">
+                        <input type="text" className="form-control" />
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-warning" data-dismiss="modal">
+                            Editar
+                        </button>
+                        <button type = "button" class = "btn btn-danger" data-dismiss="modal">
+                            Cerrar
+                        </button>
+                    </div>
+
+
+
+                </div>
+            </div>
+
+        </div>
+
+    </Fragment>;
 };
 
 export default EditTodo;
