87d5f5b4c5add90fc0c7fee1eae317fbc0a882ce
Pedro Donoso <pedro.donoso.r@usach.cl>
2022-03-03 13:23:31 -0300

    edicion completa

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -32,16 +32,18 @@ const EditTodo = ({ todo }) => {
             
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"
           // eslint-disable-next-line
-                id={`id${todo.todo_id}`}>
+                id={`id${todo.todo_id}`}
+               onClick={()=>setDescription(todo.description)}
+        >
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close" onClick={()=>setDescription(todo.description)}>
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-                        <div class="modal-body"><input type='text' className="form-control" value={description} onChange={e => setDescription(e.target.value)}/>
+<div class="modal-body"><input type='text' className="form-control" value={description} onChange={e => setDescription(e.target.value)}/>
       
       </div>
       <div class="modal-footer">
